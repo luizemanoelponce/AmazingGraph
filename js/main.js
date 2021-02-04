e0e44808e740c68e2de74bb39096b7ff60308847
Luiz Emanoel <luizemanoelponce@outlook.com.br>
2021-02-04 15:02:26 -0300

    aplicacao_finalizada

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function responsivo(){
+    var menu = document.getElementById("topnav");
+
+    if (menu.className === "navbar") {
+
+        menu.className = " responsivo";
+
+    } else {
+
+        menu.className = "navbar";
+
+    }
+}
